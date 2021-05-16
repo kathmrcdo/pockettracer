f88ca9e742ce55c2b7a4fe4742f5c2dd4823c6a6
kathmrcdo <83403685+kathmrcdo@users.noreply.github.com>
2021-05-16 19:40:03 +0800

    Add files via upload

---
+++ dbo.Transaction.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Transaction] (
+    [transactionID] INT            IDENTITY (1, 1) NOT NULL,
+    [userID]        INT            NOT NULL,
+    [Date]          DATETIME2 (7)  NOT NULL,
+    [Name]          NVARCHAR (MAX) NULL,
+    [Amount]        FLOAT (53)     NOT NULL,
+    [Category]      NVARCHAR (MAX) NOT NULL,
+    [Note]          NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED ([transactionID] ASC)
+);
+
